bbc34444ba6139447b7b3183c5582dbd526a15d5
CarlaPonciano <si.carla.ponciano@outlook.com>
2021-10-20 15:31:44 -0300

    Backend / documentation

---
+++ backend/src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'postgres',
+    database: 'tcc',
+    define: {
+        timestamps: true,
+        underscored: true,
+    }
+}
